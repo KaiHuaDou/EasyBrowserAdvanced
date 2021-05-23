5fdae11612864d41683bbb7d5aa62c40e2774fa9
KaiHuaDou <KaiHuaDou@FkqSoft.com>
2021-05-23 14:25:45 +0800

    添加了READMD.md的更新说明

--- 极简浏览器Cef/Properties/Settings.Designer.cs
+++ 极简浏览器Cef/Properties/Settings.Designer.cs
@@ -25,7 +25,7 @@ namespace 极简浏览器.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("3.3.1.9")]
+        [global::System.Configuration.DefaultSettingValueAttribute("3.3.1.10")]
         public string Version {
             get {
                 return ((string)(this["Version"]));
