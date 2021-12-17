9e79020ba2841c372d379c01e83083c004c4e97b
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2021-12-17 20:57:44 +0800

    添加了执行Javascript模块

---
+++ 极简浏览器Cef/Dialog/RunJavascript.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using 极简浏览器.Api;
+
+namespace 极简浏览器
+{
+    /// <summary>
+    /// RunJavascript.xaml 的交互逻辑
+    /// </summary>
+    public partial class RunJavascript : Window
+    {
+        public RunJavascript()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            BrowserCore.RunJavaSript(JsCode.Text);
+        }
+    }
+}
