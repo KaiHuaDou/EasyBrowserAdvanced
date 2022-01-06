a748d74f1b4ca2ad3a3e3ad684c88bcf280848d1
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2022-01-06 16:16:50 +0800

    干掉了一些没用的文件/函数

--- 极简浏览器Cef/Api/DownloadApi.cs
+++
@@ -1,3 +0,0 @@
-﻿namespace 极简浏览器.Api
-{
-}
--- 极简浏览器Cef/Api/FileApi.cs
+++ 极简浏览器Cef/Api/FileApi.cs
@@ -1,96 +1,9 @@
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Windows.Controls;
 
 namespace 极简浏览器.Api
 {
-    public enum FileType
-    {
-        History = 0,
-        BookMark = 1,
-    }
     public static class FileApi
     {
-        public static string ParseFileType(FileType fileType)
-        {
-            if(fileType == FileType.History)
-            {
-                return FilePath.HistoryPath;
-            }
-            if(fileType == FileType.BookMark)
-            {
-                return FilePath.BookMarkPath;
-            }
-            return null;
-        }
-        public static string ParseContent(string title, string url)
-        {
-            return string.Format("{0,-50}{1,-50}{2,-20}\n", title, url, DateTime.Now.ToString( ));
-        }
-        public static bool Write(string title, string url, FileType fileType)
-        {
-            try
-            {
-                if (App.Program.arguments.isNotLogging == true)
-                    goto skip;
-                if (url == "about:blank")
-                    title = "新标签页";
-                File.AppendAllText(ParseFileType(fileType), ParseContent(title, url));
-            }
-            catch(Exception e)
-            {
-                Logger.Log(e, logType: LogType.Debug, shutWhenFail: false);
-                return false;
-            }
-            skip:
-            return true;
-        }
-        public static bool Write(string text, FileType fileType)
-        {
-            try
-            {
-                if (App.Program.arguments.isNotLogging == true)
-                    goto skip;
-                File.AppendAllText(ParseFileType(fileType), text + "\n");
-            }
-            catch (Exception e)
-            {
-                Logger.Log(e, logType: LogType.Debug, shutWhenFail: false);
-                return false;
-            }
-            skip:
-            return true;
-        }
-        public static bool Clear(FileType fileType)
-        {
-            try
-            {
-                File.WriteAllText(ParseFileType(fileType), "");
-            }
-            catch(Exception e)
-            {
-                Logger.Log(e ,logType: LogType.Debug, shutWhenFail: false);
-                return false;
-            }
-            return true;
-        }
-        public static List<CheckBox> ReadAll(FileType fileType)
-        {
-            List<CheckBox> list = new List<CheckBox>( );
-            CheckBox checkBox = new CheckBox();
-            FileStream fs = new FileStream(ParseFileType(fileType), FileMode.OpenOrCreate, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            while (sr.EndOfStream != true)
-            {
-                checkBox = new CheckBox( );
-                checkBox.Content = sr.ReadLine( );
-                if (string.IsNullOrEmpty((string)checkBox.Content) == true)
-                    continue;
-                list.Add(checkBox);
-            }
-            return list;
-        }
         public static string GetStartupPath(bool isnew)
         {
             string result = "";
@@ -112,17 +25,6 @@ namespace 极简浏览器.Api
                 }
             }
             return result;
-        }
-        public static string GetLastData(FileType fileType)
-        {
-            string lastData = "";
-            FileStream fs = new FileStream(ParseFileType(fileType), FileMode.OpenOrCreate, FileAccess.Read);
-            StreamReader sr = new StreamReader(fs);
-            while(sr.EndOfStream != true)
-            {
-                lastData = sr.ReadLine( );
-            }
-            return lastData;
         }
     }
 }
