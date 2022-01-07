17de21ce448589fa4ebefa6e4cf4ab382e5f18e2
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2022-01-07 09:40:05 +0800

    精简项目文件

--- 极简浏览器Cef/Api/StandardApi.cs
+++ 极简浏览器Cef/Api/StandardApi.cs
@@ -38,16 +38,5 @@ namespace 极简浏览器.Api
             webSource = new WebSource(x);
             webSource.Show( );
         }
-        public static void ShowNotifyIcon(string text, ToolTipIcon icon = ToolTipIcon.Info, int timeOut = 2000)
-        {
-            NotifyIcon _NI = new NotifyIcon( );
-            _NI.BalloonTipIcon = icon;
-            _NI.BalloonTipText = Properties.Resources.File_Error;
-            _NI.BalloonTipTitle = Properties.Resources.BrowserName;
-            _NI.Text = text;
-            _NI.Visible = true;
-            _NI.Icon = new Icon("favicon.ico");
-            _NI.ShowBalloonTip(2000);
-        }
     }
 }
