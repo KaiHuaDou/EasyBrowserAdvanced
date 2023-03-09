4839a19c1e84053624b3d35f1719911a1aae241b
KaiHuaDou <74781268+KaiHuaDou@users.noreply.github.com>
2023-03-09 21:11:03 +0800

    更新了界面样式

--- 极简浏览器/Dialog/Report.xaml.cs
+++ 极简浏览器/Dialog/Report.xaml.cs
@@ -7,21 +7,12 @@ namespace 极简浏览器
     /// </summary>
     public partial class Report : Window
     {
-        string message;
         public Report(string msg)
         {
             InitializeComponent( );
-            message = msg;
+            textBox.Text = msg;
         }
 
-        private void button1_Click(object sender, RoutedEventArgs e)
-        {
-            App.Current.Shutdown( );
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            textBox.Text = message;
-        }
+        private void ShutdownClick(object sender, RoutedEventArgs e) => Application.Current.Shutdown( );
     }
 }
